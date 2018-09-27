09a8bb66eb233a47d91111f5682e768239a43ce1
Huang Zixun <42267068+zixuneer@users.noreply.github.com>
2018-09-27 10:03:32 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,97 @@
 function setup() {
-	// define artboard size
-	createCanvas(500,500);
-	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+  createCanvas(800, 600);
+
 }
 
+var x=0;
+var y=500;
+var speed=1.5;
+
 function draw() {
-	background(220);
 
-	//Neck
-	stroke(0);
-	line(250, 285, 250, 325);
+  background(249, 231, 159);
+  fill(247, 220, 111);
+  rect(0,600,2000,300);
+  //wheel
 
-	//arms
-	stroke(150);
-	line(200,350,170,410);
-	line(300,350,330,410);
+  rectMode(CENTER);
+  strokeWeight(3);
+  stroke(245, 176, 65);
+  fill(248, 196, 113);
+  rect(x,y,350,50,80);
 
-	//Body
-	strokeWeight(2);
-	stroke(2525);
-	fill(random(100,220),random(120,250),random(150,250),90);
-	rect(200, 325, 100, 140);
+  strokeWeight(5);
+  stroke(220, 118, 51);
+  fill(252, 243, 207);
+  ellipse(x,y,30,30);
+  ellipse(x-100,y,30,30);
+  ellipse(x-50,y,30,30);
+  ellipse(x+50,y,30,30);
+  ellipse(x+100,y,30,30);
 
-	//Wheel
-	fill(200,160,240,230);
-	noStroke();
-	ellipse(250, 465, 70, 70);
+  //leg
 
-	//Head
-	fill(250);
-	ellipse(250, 240, 90, 90);
+  rectMode(CORNERS);
+  noStroke();
+  fill(156, 100, 12)
+  rect(x-15,y-25,x+15,y-90);
 
-	//eyes
-	fill(random(0,100),random(0,100),random(0,50));
-	ellipse(230,240,16,32);
-	ellipse(270,240,16,32);
+  //body
+
+  fill(115, 198, 182)
+  rect(x-120,y-240,x+120,y-90,20);
+  fill(random(220,240),148, 138);
+  ellipse(x,y-160,60);
+
+  //arms
+  ellipseMode(CENTER);
+  fill(241, 148, 138);
+  ellipse(x-130,y-220,20,20);
+	ellipse(x+130,y-220,20,20);
+
+  fill(248, 196, 113);
+  rect(x-160,y-220,x-137,y-120,70);
+  rect(x+137,y-220,x+160,y-120,70);
+
+  fill(115, 198, 182);
+  arc(x+148,y-100, 40, 40, PI,0, CHORD);
+  arc(x-148,y-100, 40, 40, PI,0, CHORD);
+  fill(169, 223, 191);
+  rect(x+137,y-160,x+160,y-170);
+  rect(x-137,y-160,x-160,y-170);
+
+  //head
+
+  rectMode(CENTER);
+  fill(248, 196, 113);
+  rect(x,y-350,10,105);
+  strokeWeight(7);
+  stroke(248, 196, 113)
+  noFill();
+  ellipse(x-10,y-410,20);
+  ellipse(x+10,y-410,20);
+  noStroke();
+  fill(169, 223, 191);
+  rect(x,y-315,190,130,10);
+
+  //eyes & teeth
+
+  strokeWeight(3);
+  stroke(249, 231, 159);
+  fill(random(50,200),random(100,250),random(50,250));
+  ellipse(x-55,y-330,20);
+  ellipse(x+50,y-330,40);
+
+  noStroke();
+  fill(249, 231, 159);
+  rect(x-40,y-280,10,10);
+  rect(x-20,y-280,10,10);
+  rect(x,y-280,10,10);
+  rect(x+20,y-280,10,10);
+  rect(x+40,y-280,10,10);
+
+  x=x+speed;
+  if(x<0 || x>width){
+    speed=speed*-1;
+  }
 }
